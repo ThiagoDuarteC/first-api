79013bce85ea8c67a5d3dc497bff139c733af0e4
ThiagoDuarteC <thiagodcorreia01@gmail.com>
2024-07-11 20:40:32 -0300

    create first servlet

---
+++ src/main/java/br/com/api/FirstServlet.java
@@ -0,0 +1,23 @@
+package br.com.api;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet(urlPatterns = {"/firstServlet", "/teste"})
+public class FirstServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        PrintWriter writer = response.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("<h1>Primeira pagina através da nossa primeira servlet!</h1>");
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+}
